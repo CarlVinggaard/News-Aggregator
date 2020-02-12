1f17243cee5ff9f0136abe962af103920386787e
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-02-12 21:10:21 +0000

    Update functions for generating front page html

--- js/main.js
+++ js/main.js
@@ -5,8 +5,16 @@ function searchEverything(query) {
     makeList(fetchData(url));
 }
 
-function showTopStories() {
-    let url = 'https://newsapi.org/v2/top-headlines?country=gb&apiKey=081f564d356d457982b0cf109a72aea8';
+function showTopStories(category = null) {
+    let url = 'https://newsapi.org/v2/top-headlines?country=gb';
+
+    if (category) {
+        url = url + '&category=' + category;
+    }
+
+    url = url + '&apiKey=081f564d356d457982b0cf109a72aea8';
+
+    console.log(url);
 
     fetchData(url);
 }
@@ -15,38 +23,37 @@ function showTopStories() {
 function fetchData(url) {
 
     axios.get(url)
-            .then(response => {
-                
-                console.log(response);
+    .then(response => {
+        
+        console.log(response);
+        renderPage(response.data.articles);
 
-                renderFrontPage(response.data.articles);
-
-            })
-            .catch(err =>
-                console.log(err)
-            );
+    })
+    .catch(err =>
+        console.log(err)
+    );
 }
 
 // Renders the contents of an array to the front page.
-function renderFrontPage(responseArray) {
+function renderPage(responseArray) {
 
     let canvas = document.createElement('div');
 
     // Each item contains a row, a col-X, an image and a paragraph.
     for (article of responseArray) {
         
-        newRow = renderFrontPageItem(article);
+        newRow = renderPageItem(article);
         canvas.appendChild(newRow);
     }
 
-    document.getElementById('frontpage').innerHTML = canvas.innerHTML;
+    document.getElementById('content').innerHTML = canvas.innerHTML;
 }
 
 // Creates an html row with the contents of an article. 
-function renderFrontPageItem(article) {
+function renderPageItem(article) {
 
     let newRow = document.createElement('div');
-    newRow.classList.add('row', 'justify-content-center', 'my-1', 'p-4');
+    newRow.classList.add('row', 'justify-content-center', 'my-1', 'pb-4');
 
     let newCol = document.createElement('div');
     newCol.classList.add('col-12', 'section-container');
@@ -58,20 +65,27 @@ function renderFrontPageItem(article) {
     let newRow2 = document.createElement('div');
     newRow2.classList.add('row');
 
+    let newLine = document.createElement('hr');
+    newLine.classList.add('col-12');
+
     // Image
     let newImgCol = document.createElement('div');
-    newImgCol.classList.add('col-12', 'col-sm-7', 'col-md-5', 'mb-2');
-
     let newImg = document.createElement('img');
-    newImg.src = article.urlToImage;
+    
+    if (article.urlToImage) {
+        newImgCol.classList.add('col-12', 'col-sm-7', 'col-md-5', 'mb-2');
+        newImg.src = article.urlToImage;
+    }
 
     // Text
     let newTextCol = document.createElement('div');
-    newTextCol.classList.add('col-12', 'col-sm-5', 'col-md-7');
+    newTextCol.classList.add('col-12');
+    article.urlToImage ? newTextCol.classList.add('col-sm-5', 'col-md-7'): null;
 
+    let date = new Date(article.publishedAt);
     let newTime = document.createElement('p');
     newTime.classList.add('published-time');
-    newTime.innerHTML = article.publishedAt;
+    newTime.innerHTML = date.toString().slice(0,21);
 
     let newHeading = document.createElement('h2');
     newHeading.innerText = removeSourceFromTitle(article.title);
@@ -85,16 +99,20 @@ function renderFrontPageItem(article) {
     newSource.innerText = article.source.name;
 
     // Put it together
-    newImgCol.appendChild(newImg);
+    if (article.urlToImage) {
+        newImgCol.appendChild(newImg);
+        newRow2.appendChild(newImgCol);
+    }
+
     newTextCol.appendChild(newTime);
     newTextCol.appendChild(newHeading);
     newTextCol.appendChild(newParagraph);
     newTextCol.appendChild(newSource);
-    newRow2.appendChild(newImgCol);
     newRow2.appendChild(newTextCol);
     newAnchor.appendChild(newRow2);
     newCol.appendChild(newAnchor);
     newRow.appendChild(newCol);
+    newRow.appendChild(newLine);
 
     return newRow;
 }
