82ae0c0346468485514cb99dd0bac903ec5128da
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-05-07 23:50:37 +0000

    Add jasmine

--- js/jasmineTests.js
+++ js/jasmineTests.js
@@ -1,1 +1,8 @@
-removeSourceFromTitle();
+describe("removeSourceFromTitle() tests", function() {
+    
+    it("Should return 'Hello'", function() {
+        let title = 'Hello - world';
+        let slicedTitle = removeSourceFromTitle(title);
+        expect(slicedTitle).toBe('Hello');
+    });
+});
